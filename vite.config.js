88f734bfaa4a58142a3cb0f9928149d37ba4c231
mgalbakri <63304783+mgalbakri@users.noreply.github.com>
2026-01-16 11:04:25 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,3 +4,14 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
     plugins: [react()],
 })
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+      plugins: [react()],
+      build: {
+              rollupOptions: {
+                        input: 'index.html'
+              }
+      }
+})
